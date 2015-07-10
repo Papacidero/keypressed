7df205b8db716dbe7b61b76d5db0fc9dc031d464
Carlos Eduardo Papacidero <papacidero@gmail.com>
2015-07-10 10:14:21 -0300

    License Info

---
+++ keypressed.js
@@ -0,0 +1,5 @@
+/*! Key Pressed - v0.0.1 - 2015-07-08
+ * https://github.com/Papacidero/inputfit
+ *
+ * Copyright (c) 2015 Carlos Eduardo Papacidero <papacidero@gmail.com>;
+ * Licensed under the MIT license */
